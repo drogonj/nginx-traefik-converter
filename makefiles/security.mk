79cf6d0f82df5045c27bb65c619139b6a7922202
drogonj <example@example.com>
2026-02-24 11:05:50 +0100

    fix: Makefile cleaning

--- makefiles/security.mk
+++ makefiles/security.mk
@@ -1,5 +1,2 @@
-scan/code: ## scans code for vulnerabilities
+scan/code: ## Scans code for vulnerabilities with Trivy
 	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy fs /nginx-traefik-converter
-
-scan/binary: ## scans binary for vulnerabilities
-	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy fs /nginx-traefik-converter/dist/nginx-traefik-converter_darwin_amd64_v1/nginx-traefik-converter
